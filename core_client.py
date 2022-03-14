556249a041d5b9f6a59420e50e80456bddc9eed0
Ygor Moreira Lima <admin@laboratoriodebits.com.br>
2022-03-14 03:42:56 -0300

    Separação do core cliente e servidor

---
+++ core_client.py
@@ -0,0 +1,17 @@
+import communications_client
+
+def emergencyStop():
+    msg = "Parada em emergência!"
+    code = "EEE"
+
+def display():
+    # Lat
+    # Long
+    # Rumo
+    # Veloc
+    # Obstáculos nas proximidades
+    # Mapa
+    return
+
+def ativarMAE():
+    return
