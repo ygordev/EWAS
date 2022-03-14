bceb0b6a0076bb12c0d88973fd3acbf90d5a40e2
Ygor Moreira Lima <admin@laboratoriodebits.com.br>
2022-03-14 03:24:30 -0300

    CREATE função mostraInformacoes(msgNMEA)

--- navigation.py
+++ navigation.py
@@ -1,5 +1,11 @@
+#############################
 # EWAS v0.1
 # Módulo de Navegação
+# Escrito por Ygor Moreira Lima
+#############################
+
+# Documentação oficial pynmea2
+# https://github.com/Knio/pynmea2
 
 import pynmea2
 
@@ -12,5 +18,11 @@ class NavegacaoGlobal(self, origemLat, origemLong, destinoLat, destinoLong):
 
 '''
 
-msg = pynmea2.parse("$GPGGA,184353.07,1929.045,S,02410.506,E,1,04,2.6,100.00,M,-33.9,M,,0000*6D")
-print(msg.lat)
+def mostraInformacoes(msgNMEA):
+    msg = pynmea2.parse(msgNMEA)
+    lat = '%02d°%07.4f′' % (msg.latitude, msg.latitude_minutes)
+    long = '%02d°%07.4f′' % (msg.longitude, msg.longitude_minutes)
+    print("Latitude: {}".format(lat))
+    print("Longitude: {}".format(long))
+    
+mostraInformacoes("$GPGGA,184353.07,1929.045,S,02410.506,E,1,04,2.6,100.00,M,-33.9,M,,0000*6D")
